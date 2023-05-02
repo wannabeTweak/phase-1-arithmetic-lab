c6b03ef541f05752a524a87931a1a09e5118f7ca
Patryk <sucheckipatryk@gmail.com>
2023-05-02 12:04:57 -0400

    All Tests Passing

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+//Test 1
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+//Test 2
+const random = Math.floor(Math.random() *10) + 1;
+console.log(random);
+
+//Test 3
+const num3 = 29
+const num4 = 5
+const mod = num3 % num4;
+console.log(mod);
+
+//Test 4
+const max = Math.max(1, 4, 6, 10, 15, 20);
+console.log(max);
